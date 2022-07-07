4b6135a6140ad824bd565d007c180854ba74bad4
microbroke <98540725+microbroke@users.noreply.github.com>
2022-07-07 15:58:33 +0200

    Update zabbixinstall_6.0.3_Ubuntu20.04LTS.sh

--- zabbixinstall_6.0.3_Ubuntu20.04LTS.sh
+++ zabbixinstall_6.0.3_Ubuntu20.04LTS.sh
@@ -11,6 +11,9 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+# Set Timezone to Europe/Stockholm (timedatectl list-timezones)
+sudo timedatectl set-timezone Europe/Stockholm
+
 # Mysql Zabbix user password
 read -p "Set a password for the mysql zabbix user:" mysqlzabbixpw
 
@@ -18,8 +21,8 @@ read -p "Set a password for the mysql zabbix user:" mysqlzabbixpw
 apt install mysql-server
 
 # a. Add exclusion for zabbix-package
-touch /etc/dpkg/dpkg.cfg.d/excludes
-echo "path-include=/usr/share/doc/zabbix*" >> /etc/dpkg/dpkg.cfg.d/excludes
+#touch /etc/dpkg/dpkg.cfg.d/excludes
+#echo "path-include=/usr/share/doc/zabbix*" >> /etc/dpkg/dpkg.cfg.d/excludes
 
 # b. Install Zabbix repository
 wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-3+ubuntu20.04_all.deb
@@ -36,6 +39,7 @@ sudo mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost;"
 
 # On Zabbix server host import initial schema and data. You will be prompted to enter your newly created password.
 #zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p$mysqlzabbixpw zabbix
+# create.sql.gz is no longer available and have moved to server.sql.gz contained within the application zabbix-sql-scripts
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -p$mysqlzabbixpw zabbix
 
 # e. Configure the database for Zabbix server
